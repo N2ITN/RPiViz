43ffa034e35b6401ab8a57d399ab040da8a4443c
Zachary A Estela <zestela@gmail.com>
2016-10-24 17:36:20 -0700

    major cleanup

--- Live/executor.py
+++ Live/executor.py
@@ -1,3 +1,5 @@
+""" main loop """
+
 from picamera import PiCamera
 from time import sleep,time
 import do_magic
@@ -5,29 +7,40 @@ from identify import crop2face
 import numpy as np
 import cv2
 import requests
-import Image
 nv = PiCamera()
 import dlib
+
+# Load in main script to reduce disk IO
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #Landmark identifier. Set the filename to whatever you named the downloaded file
+
+# Camera rotation and resolution options
 #nv.rotation = 180
 #nv.resolution = (480, 360)
 #nv.resolution = (960, 540)
 nv.resolution = (1280, 720)
 #nv.resolution = (1920, 1080)
+
+
+# TODO: Remove code for local network alerts
 try:
     requests.get('http://192.168.43.45:3000/fan/off')
     requests.get('http://192.168.43.45:3000/lght/off')
 except Exception as e:
     print e
 
+    
+
 def camera_loop(loops,nap):
+    """ Orchestrator of all """
     buffer = []
     for loop in xrange(0,loops):
         snap = '/home/pi/Desktop/snap.jpg'
         start = time()
         nv.capture(snap) 
         cal = .40
-        print time() - start, "starting dlib"
+        
+        
+        # TODO: Function here
         try:
             shape,frame = crop2face(snap,predictor)
         except TypeError:
@@ -38,16 +51,17 @@ def camera_loop(loops,nap):
            print "no face detected"
            continue
         perc_asleep = round((analysis / cal) * 100,2)    
-        #r = requests.get('https://wt-joseherminiocollas-goatstone-com-0.run.webtask.io/set-sleep?webtask_no_cache=1&level='+str(perc_asleep))
         
+        # TODO: Function
         buffer.append(perc_asleep)
         print buffer
         a = False
         vector = int(np.mean(buffer))
         defcon = (255,255,255)
         alertness = 'calibrating'
+        
+        # TODO: Functionalize as "alert", create option for network alert or preview window
         if len(buffer) > 1:
-            
             try:
                 if int(vector) > 85:
                     defcon = (0,255,0)
@@ -56,13 +70,11 @@ def camera_loop(loops,nap):
                     requests.get('http://192.168.43.45:3000/lght/off')
                     
                 elif int(vector) > 75:
-                    alertness = 'okay'
-                    
+                    alertness = 'okay' 
                     requests.get('http://192.168.43.45:3000/fan/on')
                     requests.get('http://192.168.43.45:3000/lght/on')
                     requests.get('https://wt-dev-screenverve_com-0.run.webtask.io/internetButton?webtask_no_cache=1')
-                    defcon = (0,255,255)
-                    
+                    defcon = (0,255,255)       
                 else:
                     defcon = (0,0,255)
                     alertness = 'danger zone'
@@ -74,14 +86,14 @@ def camera_loop(loops,nap):
                 print e
             buffer.pop(0)
              
-        print time()-start, 'total seconds'
+       
+        # TODO: Functionalize as "preview_window"
         text = ' '.join(['ratio',  str(analysis)])
         text2 = ' '.join(['alertness:', alertness])#, str(vector)])
         cv2.destroyAllWindows()
         img = cv2.imread("/home/pi/Desktop/snap_draw.jpg")
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img,text2,(10,30), font, .5,defcon,1,cv2.CV_AA)
-        
+        cv2.putText(img,text2,(10,30), font, .5,defcon,1,cv2.CV_AA)  
         if a:
             cv2.putText(img,'sending ALERT',(10,50), font, .5,(0,0,255),1,cv2.CV_AA)
         cv2.namedWindow('z', cv2.WND_PROP_FULLSCREEN)          
@@ -90,6 +102,7 @@ def camera_loop(loops,nap):
         cv2.waitKey(1000)
 
         
+        print time()-start, 'total seconds'
         print
         print
 camera_loop(200,0)
