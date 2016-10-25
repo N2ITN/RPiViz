aaea083acc7718e0ac39435d961a24bc210d874e
Zachary A Estela <zestela@gmail.com>
2016-10-24 17:13:55 -0700

    Cleanup

--- Live/identify.py
+++ Live/identify.py
@@ -2,13 +2,14 @@ import cv2
 import dlib
 import numpy as np
 def crop2face(pic,predictor):
+    
+    ''' Find face, create DLib rectangle around it'''
     img  = cv2.imread(pic)
-
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     clahe_image = clahe.apply(gray)
-    equ = cv2.equalizeHist(gray)
+    #equ = cv2.equalizeHist(gray) <--- more drastic contrast boosting, doesn't work evenly across lighting conditions
     faceprime = face_cascade.detectMultiScale(clahe_image, 1.3, 5)
     print faceprime
     grow = (30)
@@ -19,17 +20,13 @@ def crop2face(pic,predictor):
         y1 = y-grow
         y2 = y+h+grow
 
-
-        # frame = dlib.rectangle(long(x1), long(y1),long(x2), long(y2))
-        # rects = dlib.rectangles(frame)
-        # SO = np.array([long(x),long(y),long(x+w),long(y+h)])
         detections = dlib.rectangle(long(x),long(y),long(x+w),long(y+h))
-        # for k,v in enumerate(rects):
-        #     print k,v
-        
-        
+
+        ''' To save cropped image '''
         #color_normal = img[y1:y2, x1:x2]
         #cv2.imwrite('snapcrop.jpg',color_normal)
+        
+        
         shape = predictor(clahe_image, detections)
         print shape
         return shape, img
