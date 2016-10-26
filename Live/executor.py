4ec46e26a274e19fc7a793ed9eff5655be8f0903
N2ITN <zestela@gmail.com>
2016-10-26 02:39:51 +0000

    display tweaks, example pics

--- Live/executor.py
+++ Live/executor.py
@@ -12,15 +12,16 @@ import dlib
 
 # Load in main script to reduce disk IO
 predictor = dlib.shape_predictor(
-    "shape_predictor_68_face_landmarks.dat"
+    "/home/pi/Desktop/shape_predictor_68_face_landmarks.dat"
 )  #Landmark identifier. Set the filename to whatever you named the downloaded file
 
 # Camera rotation and resolution options
 #nv.rotation = 180
 #nv.resolution = (480, 360)
 #nv.resolution = (960, 540)
-nv.resolution = (1280, 720)
+#nv.resolution = (1280, 720)
 #nv.resolution = (1920, 1080)
+nv.resolution = (2592,1944)
 
 # TODO: Remove code for local network alerts
 '''
@@ -39,7 +40,7 @@ def camera_loop(loops, nap):
         snap = '/home/pi/Desktop/snap.jpg'
         start = time()
         nv.capture(snap)
-        cal = .40
+        cal = .370
 
         # TODO: Function here
         try:
@@ -87,23 +88,24 @@ def camera_loop(loops, nap):
                 print e
             buffer.pop(0)
             print alertness
-    '''
+    
     # TODO: Functionalize as "preview_window"
-        #text = ' '.join(['ratio', str(analysis)])
-        #text2 = ' '.join(['alertness:', alertness])  #, str(vector)])
-        #cv2.destroyAllWindows()
-        #img = cv2.imread("snap_draw.jpg")
-        #font = cv2.FONT_HERSHEY_SIMPLEX
-        #cv2.putText(img, text2, (10, 30), font, .5, defcon, 1, cv2.CV_AA)
-        #if a:
-           #cv2.putText(img, 'sending ALERT', (10, 50), font, .5, (0, 0, 255),
+        text = ' '.join(['ratio', str(analysis)])
+        text2 = ' '.join(['alertness:', alertness])  #, str(vector)])
+        cv2.destroyAllWindows()
+        img = cv2.imread("snap_draw.jpg")
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, text2, (10, 30), font, .5, defcon, 1, cv2.CV_AA)
+        if a:
+           cv2.putText(img, 'sending ALERT', (10, 50), font, .5, (0, 0, 255),
                         1, cv2.CV_AA)
-        #cv2.namedWindow('z', cv2.WND_PROP_FULLSCREEN)
-        #cv2.setWindowProperty('z', cv2.WND_PROP_FULLSCREEN,
+        cv2.namedWindow('z', cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('z', cv2.WND_PROP_FULLSCREEN,
                               cv2.cv.CV_WINDOW_FULLSCREEN)
-       # cv2.imshow('z', img)
-       # cv2.waitKey(1000)
-    '''
+        cv2.imshow('z', img)
+        cv2.imwrite('alert.jpg',img)
+        cv2.waitKey(1000)
+    
     print time() - start, 'total seconds'
     print
     print
