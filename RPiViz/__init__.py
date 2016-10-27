e7e099824562802a4b4ce9124a34b728824dc301
Zachary A Estela <zestela@gmail.com>
2016-10-26 21:51:29 -0700

    __main__.py tweaks

--- RPiViz/__init__.py
+++ RPiViz/__init__.py
@@ -1,2 +1,2 @@
 """ Apache License 2.0 Zach Estela github.com/N2ITN """
-__version__='0.0.10'
+__version__='0.0.11'
--- RPiViz/__main__.py
+++ RPiViz/__main__.py
@@ -1,8 +1,9 @@
 import RPiViz as vz
 import sys
-import executor
+
 import deps
 if sys.argv[1] == 'setup':
 	deps.main()
 elif sys.argv[1] == 'start':
+	import executor
 	executor()
