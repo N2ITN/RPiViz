43c95313c85489d86b77228d67385d51de881612
Zachary A Estela <zestela@gmail.com>
2016-10-27 16:36:00 -0700

    testing

--- get_reqs.sh
+++ get_reqs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-echo starting installation
-echo installing opencv
-sudo apt-get install libopencv-dev python-opencv
-echo installing python requirements
+
+set -o errexit
+set -o pipefail
+
 sudo cat requirements.txt | xargs pip install
-echo installing libboost-python-dev
-sudo apt-get install libboost-python-dev
-echo installing dlib, this will take a long time
+sudo apt-get install libopencv-dev python-opencv libboost-python-dev
 sudo pip install dlib==19.1.0
 
