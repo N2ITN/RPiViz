6a0506196b0a3f01d7bddb3d2144b612ae2b6097
Zachary A Estela <zestela@gmail.com>
2016-10-27 16:49:39 -0700

    test

--- get_reqs.sh
+++ get_reqs.sh
@@ -2,7 +2,8 @@
 
 set -o errexit
 set -o pipefail
-
+sudo apt-get update
+sudo apt-get upgrade
 sudo cat requirements.txt | xargs pip install
 sudo apt-get install libopencv-dev python-opencv libboost-python-dev
 sudo pip install dlib==19.1.0
