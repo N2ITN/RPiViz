30b3c999acc68c86f50425e78fe7345872dd3a37
Zachary A Estela <zestela@gmail.com>
2016-10-28 16:59:23 -0700

    sudo all the things

--- get_reqs.sh
+++ get_reqs.sh
@@ -7,4 +7,4 @@ sudo apt-get upgrade
 sudo cat requirements.txt | xargs pip install
 sudo apt-get install libopencv-dev python-opencv libboost-python-dev
 sudo pip install dlib==19.1.0
-
+python executor.py
