40302b6a2ddbab0a6b058212b46c313a9b7c9daa
Zachary A Estela <zestela@gmail.com>
2016-10-29 08:07:50 -0700

    add 'tight' edge detect to identify.py

--- raspiviz/identify.py
+++ raspiviz/identify.py
@@ -34,6 +34,9 @@ def crop2face(pic, predictor):
         color_normal = img[y1:y2, x1:x2]
         cv2.imwrite('snapcrop.jpg', color_normal)
 
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+        tight = cv2.Canny(blurred, 225, 250)
+
         # Detect face landmarks with dlib rectangle, dlib shape predictor
-        shape = predictor(clahe_image, detections)
-        return shape, clahe_image
+        shape = predictor(tight, detections)
+        return shape, tight
